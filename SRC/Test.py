3a5f85af1ce50cd03074d54b6c072fcabd9a73f9
MaxKreitzer <201699@pennmanor.net>
2021-03-19 09:03:22 -0400

    update 7

--- SRC/Test.py
+++ SRC/Test.py
@@ -1,18 +1,29 @@
-import os
-import time
-#print("Would you like to run FixWifi")
-#if str(input()) == "YES":
-#    os.system("nmcli radio wifi off")
-  #  print("wifi now disabled")
- #   time.sleep(5)
- #   print("Re-enabling wifi")
-#    os.system("nmcli radio wifi on")
-#else:
-#    print("FixWifi did not run.")
-
 #!/usr/bin/python3
 import tkinter as tk
-win = tk.Tk()
-win.title("Howdy")
-mylabel=tk
-win.mainloop()
+#import os
+#import time
+#win = tk.Tk()
+#win.title("FixWifi")
+#print("Would you like to run FixWifi")
+#mylabel = tk.Label(win, text="Would you like to run FixWifi")
+#if str(input()) == "YES":
+#    os.system("nmcli radio wifi off")
+#    #print("wifi now disabled")
+#    mylabel = tk.Label(win, text="wifi now disabled")
+#    time.sleep(5)
+    #print("Re-enabling wifi")
+#    mylabel = tk.Label(win, text="Re-enabling wifi")
+#    os.system("nmcli radio wifi on")
+#else:
+    #print("FixWifi did not run.")
+#    mylabel = tk.Label(win, text="FixWifi did not run.")
+#win.mainloop()
+
+#!/usr/bin/python3
+#import tkinter as tk
+#win = tk.Tk()
+#win.title("Howdy")
+#mylabel=tk.Label(win,text = "Hello",font=("comforta",60,"bold"))
+#mylabel.pack()
+#win.mainloop()
+
