3fe5128c8bcb7c30381e1c33a8120d43b4f4c128
MaxKreitzer <201699@pennmanor.net>
2021-03-19 08:28:40 -0400

    update 5

--- SRC/Test.py
+++ SRC/Test.py
@@ -1,11 +1,18 @@
 import os
 import time
-print("Would you like to run FixWifi")
-if str(input()) == "YES":
-    os.system("nmcli radio wifi off")
-    print("wifi now disabled")
-    time.sleep(10)
-    os.system("nmcli radio wifi on")
-    print("wifi reestablished")
-else:
-    print("ok")
+#print("Would you like to run FixWifi")
+#if str(input()) == "YES":
+#    os.system("nmcli radio wifi off")
+  #  print("wifi now disabled")
+ #   time.sleep(5)
+ #   print("Re-enabling wifi")
+#    os.system("nmcli radio wifi on")
+#else:
+#    print("FixWifi did not run.")
+
+#!/usr/bin/python3
+import tkinter as tk
+win = tk.Tk()
+win.title("Howdy")
+
+win.mainloop()
