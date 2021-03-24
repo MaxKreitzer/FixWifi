16a91800acda8019623228c7b14cc5f74ef7b52c
MaxKreitzer <201699@pennmanor.net>
2021-03-24 01:19:24 -0400

    update 12

--- SRC/FixWifi.py
+++ SRC/FixWifi.py
@@ -1,43 +1,38 @@
 #!/usr/bin/python3
 import tkinter as tk
-#import os
-#import time
+
 win = tk.Tk()
+win.resizable(0, 0)
 win.title("FixWifi")
 
 mylabel = tk.Label(win, text="Would you like to run FixWifi?")
 mylabel.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-def buttonclick():
+def buttonyes():
+    import os
+
     yesButton.destroy()
     noButton.destroy()
     mylabel.destroy()
-
     Disable = tk.Label(win, text="Disabling wifi please wait...")
     Disable.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
-    win.after(5000)
+    os.system("nmcli radio wifi off")
+
+    while os.popen("nmcli networking connectivity", "r", 1).readline(1) == "n":
+        os.system("nmcli radio wifi on")
     Disable.destroy()
 
-    Enable = tk.Label(win, text="Re-enabling Wifi...")
+    Enable = tk.Label(win, text="Wifi Re-enabled, You're ready to go!")
     Enable.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-yesButton = tk.Button(win, text="Yes", fg=("green"), padx=40, pady=20, command=buttonclick)
-noButton = tk.Button(win, text="No", fg=("red"), padx=40, pady=20, command=buttonclick)
+def buttonno():
+    win.destroy()
+
+yesButton = tk.Button(win, text="Yes", fg=("green"), padx=40, pady=20, command=buttonyes)
+noButton = tk.Button(win, text="No", fg=("red"), padx=40, pady=20, command=buttonno)
 
 yesButton.grid(row=2, column=1)
 noButton.grid(row=2,column=2)
-
-#if str(input()) == "YES":
-#    os.system("nmcli radio wifi off")
-#    #print("wifi now disabled")
-#    mylabel = tk.Label(win, text="wifi now disabled")
-#    time.sleep(5)
-    #print("Re-enabling wifi")
-#    mylabel = tk.Label(win, text="Re-enabling wifi")
-#    os.system("nmcli radio wifi on")
-#else:
-    #print("FixWifi did not run.")
-#    mylabel = tk.Label(win, text="FixWifi did not run.")
 
 win.mainloop()
 
